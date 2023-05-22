69bd07cd87ace37a587d18d47e0b47866efd0932
mwendwa-will <williammwendwa@gmail.com>
2023-05-22 14:33:42 +0300

    made a change to the shell.h

--- shell.h
+++ shell.h
@@ -18,9 +18,9 @@
 /* Global environemnt */
 extern char **environ;
 /* Global program name */
-char *name;
+extern char *name;
 /* Global history counter */
-int hist;
+extern int hist;
 
 /**
  * struct list_s - A new struct type defining a linked list.
@@ -58,7 +58,7 @@ typedef struct alias_s
 } alias_t;
 
 /* Global aliases linked list */
-alias_t *aliases;
+extern alias_t *aliases;
 
 /* Main Helpers */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
